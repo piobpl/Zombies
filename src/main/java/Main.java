5b6331529545f3cf77072ffc51d2ee2118eaa6bd
piobpl <piotrb10@gmail.com>
2013-04-28 19:40:00 +0200

    Taki sobie commit
    Change-Id: I78b2e2c53ee7cc18b23e410dbafe63ddaa413f6a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Zomfbiaki!!!");
 		System.out.println("Roger that@Karol");
+		System.out.println("No pullnij sie, no");
 	}
 
 }
