4f0a2af667351241491f2ba9e10f98142279c338
piobpl <piotrb10@gmail.com>
2013-04-28 18:58:03 +0200

    !

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Zomfbiaki");
+		System.out.println("Zomfbiaki!");
 		System.out.println("Roger that@Karol");
 	}
 
