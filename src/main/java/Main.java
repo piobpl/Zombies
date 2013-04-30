2eb38bb4904ca393b7db9d3ddf5250d6ef5f2216
Edoipi <Edoipi@Laptop>
2013-04-30 14:45:01 +0200

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("1 2 3 próba system outa");
-		
 		System.out.println("Zomfbiaki");
 		System.out.println("Roger that@Karol");
 		System.out.println("Pull z laptopa");
