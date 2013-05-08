1969caa2b849ca85fa4ee4cabe4fd226c1ef7617
jerzozwierz <jerzozwierz@jerzozwierz>
2013-05-08 20:05:49 +0200

    jop jop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
-
+	/**
+	 * Fucking awesome main function
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("1 2 3 próba system outa <- ktos to napisal na windowsie? jezeli tak to chyba trzeba ustawic kodowanie utf8");
 		System.out.println("Zomfbiaki");
@@ -9,6 +12,7 @@ public class Main {
 		System.out.println("a tera ze stogu siana w stodole");
 		System.out.println("patataj");
 		System.out.println("no witam");
+		System.out.println("pykam jeszcze raz, dodalem doskonalego javadoca");
 	}
 
 }
