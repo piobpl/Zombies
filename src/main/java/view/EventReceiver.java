3ea2643bdbddebf3fc5abb4a8d828e103a291a65
piobpl <piotrb10@gmail.com>
2013-05-09 19:02:43 +0200

    Zalążek EventReceiver

--- src/main/java/view/EventReceiver.java
+++ src/main/java/view/EventReceiver.java
@@ -1,16 +1,24 @@
 package view;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 import model.Player;
 import utility.Pair;
 
+/**
+ * Klasa służaca do komunikacji z użytkownikiem.
+ * 
+ * @author piob
+ */
 public class EventReceiver {
-		
+
 	public static enum EventType {
 		BoardClicked, HandClicked, ApplyButtonClicked, CancelButtonClicked,
 	}
 
 	public abstract static class Event {
-		public EventType type;
+		public final EventType type;
 
 		public Event(EventType type) {
 			this.type = type;
@@ -18,20 +26,23 @@ public class EventReceiver {
 	}
 
 	public static class BoardClickedEvent extends Event {
-		public BoardClickedEvent() {
-			super(EventType.BoardClicked);
-		}
+		public final Pair<Integer, Integer> cardClicked;
 
-		Pair<Integer, Integer> cardClicked;
+		public BoardClickedEvent(Pair<Integer, Integer> cardClicked) {
+			super(EventType.BoardClicked);
+			this.cardClicked = cardClicked;
+		}
 	}
 
 	public static class HandClickedEvent extends Event {
-		public HandClickedEvent() {
-			super(EventType.HandClicked);
-		}
+		public final Integer cardClicked;
+		public final Player player;
 
-		Integer cardClicked;
-		Player player;
+		public HandClickedEvent(Integer cardClicked, Player player) {
+			super(EventType.HandClicked);
+			this.cardClicked = cardClicked;
+			this.player = player;
+		}
 	}
 
 	public static class ApplyButtonClickedEvent extends Event {
@@ -46,16 +57,31 @@ public class EventReceiver {
 		}
 	}
 
-	public final GUI gui;
-	
-	public EventReceiver(GUI gui){
-		this.gui = gui;
-	}
-	
-	// TODO
+	@SuppressWarnings("unused")
+	private final GUI gui;
+	private final BlockingQueue<Event> eventQueue;
 
+	public EventReceiver(GUI gui) {
+		this.gui = gui;
+		eventQueue = new ArrayBlockingQueue<>(8);
+	}
+
+	// TODO - lapanie eventow
+
+	/**
+	 * Funkcja zwraca kolejną akcje wykonaną przez użytkownika. Jeżeli
+	 * użytkownik nie podjął żadnego działania, blokuje wątek w oczekiwaniu.
+	 * Jeżeli zostanie przerwana, wypisuje o tym informacje na stderr i zwraca
+	 * null.
+	 */
 	public Event getNextEvent() {
-		return null;
+		try {
+			return eventQueue.take();
+		} catch (InterruptedException e) {
+			System.err.println("Czekanie na kolejny event zostalo przerwane:");
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
