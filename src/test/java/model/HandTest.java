a09e9f457ea80776d51e290ef2aae1fd7d4b6693
piobpl <piotrb10@gmail.com>
2013-05-11 17:44:59 +0200

    Przykladowy test

---
+++ src/test/java/model/HandTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import cards.zombies.Zombie;
+
+public class HandTest {
+
+	@Test
+	public void test() {
+		Hand hand = new Hand();
+		for(int i = 0; i < 4; ++i)
+			assertEquals(null, hand.get(i));
+		Card[] cards = new Card[4];
+		for(int i = 0; i < 4; ++i){
+			cards[i] = new Zombie(i);
+			hand.set(i, cards[i]);
+		}
+		for(int i = 0; i < 4; ++i)
+			assertEquals(cards[i], hand.get(i));
+	}
+
+}
