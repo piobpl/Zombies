b056616dae3f65037a73a0e2b0ff870007810bba
michal <michal@zajac-komp>
2013-05-12 23:56:41 +0200

    Mover - sprawdzanie GameOver

--- src/main/java/cards/helpers/Mover.java
+++ src/main/java/cards/helpers/Mover.java
@@ -2,6 +2,8 @@ package cards.helpers;
 
 import model.Card;
 import model.GameState;
+import model.Player;
+import controller.GameOver;
 
 /**
  * Klasa wspomagająca ruszanie zombich i psów po planszy.
@@ -13,8 +15,9 @@ import model.GameState;
 // TODO : sprawdzanie czy pole puste, nie tylko isEmpty
 public abstract class Mover {
 	public static boolean moveForward(GameState gameState, int x, int y) {
-		if(x == 4 || !gameState.getBoard().isEmpty(x+1, y)) return false;
 		Card card = gameState.getBoard().get(x, y);
+		if(x == 4 && card.getName().equals("Zombie")) throw new GameOver(Player.ZOMBIE);
+		if(!gameState.getBoard().isEmpty(x+1, y)) return false;
 		gameState.getBoard().set(x+1, y, card);
 		gameState.getBoard().set(x, y, null);
 		return true;
