7ce2ec46ba4e312176f563d2c9f614537a1daac8
michal <michal@zajac-komp>
2013-05-12 23:37:33 +0200

    Klasa Mover

---
+++ src/main/java/cards/helpers/Mover.java
@@ -0,0 +1,43 @@
+package cards.helpers;
+
+import model.Card;
+import model.GameState;
+
+/**
+ * Klasa wspomagająca ruszanie zombich i psów po planszy.
+ * @author michal
+ *
+ */
+
+
+// TODO : sprawdzanie czy pole puste, nie tylko isEmpty
+public abstract class Mover {
+	public static boolean moveForward(GameState gameState, int x, int y) {
+		if(x == 4 || !gameState.getBoard().isEmpty(x+1, y)) return false;
+		Card card = gameState.getBoard().get(x, y);
+		gameState.getBoard().set(x+1, y, card);
+		gameState.getBoard().set(x, y, null);
+		return true;
+	}
+	public static boolean moveBackward(GameState gameState, int x, int y) {
+		if(x == 0 || !gameState.getBoard().isEmpty(x-1, y)) return false;
+		Card card = gameState.getBoard().get(x, y);
+		gameState.getBoard().set(x-1, y, card);
+		gameState.getBoard().set(x, y, null);
+		return true;
+	}
+	public static boolean moveLeft(GameState gameState, int x, int y) {
+		if(y == 0 || !gameState.getBoard().isEmpty(x, y-1)) return false;
+		Card card = gameState.getBoard().get(x, y);
+		gameState.getBoard().set(x, y-1, card);
+		gameState.getBoard().set(x, y, null);
+		return true;
+	}
+	public static boolean moveRight(GameState gameState, int x, int y) {
+		if(y == 2 || !gameState.getBoard().isEmpty(x, y+1)) return false;
+		Card card = gameState.getBoard().get(x, y);
+		gameState.getBoard().set(x, y+1, card);
+		gameState.getBoard().set(x, y, null);
+		return true;
+	}
+}
