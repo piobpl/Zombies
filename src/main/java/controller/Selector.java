b53eb15d83b7cd6cdc0d00508d450c7e36d406c5
piobpl <piotrb10@gmail.com>
2013-05-14 21:14:50 +0200

    Poprawione #25

--- src/main/java/controller/Selector.java
+++ src/main/java/controller/Selector.java
@@ -39,71 +39,74 @@ public class Selector {
 		Selection current = null, candidate = null;
 		int currentRate = 0, candidateRate;
 		Board board = gameState.gui.getBoard();
+		Event e = null;
+		BoardClickedEvent f = null;
+		HandClickedEvent g = null;
+		ButtonClickedEvent h = null;
 		while (true) {
-			Event e = eventReceiver.getNextEvent();
+			e = eventReceiver.getNextEvent();
 			if (e.type == EventType.ButtonClicked) {
-				ButtonClickedEvent f = (ButtonClickedEvent) e;
-				if (f.button == Button.ApplySelection)
+				h = (ButtonClickedEvent) e;
+				if (h.button == Button.ApplySelection)
 					return currentRate == 2 ? current : null;
-				else if (f.button == Button.CancelSelection)
+				else if (h.button == Button.CancelSelection)
 					return null;
 				else
 					continue;
+			} else if (e.type == EventType.HandClicked) {
+				if (card.getSelectionType() != SelectionType.HAND)
+					continue;
+				g = (HandClickedEvent) e;
+				candidate = new HandSelection(g.player, g.cardClicked);
+			} else {
+				if (card.getSelectionType() == SelectionType.HAND)
+					continue;
+				f = (BoardClickedEvent) e;
+				if (current == null)
+					candidate = selectionMap.get(card.getSelectionType()).add(
+							f.cardClicked);
+				else
+					candidate = current.add(f.cardClicked);
 			}
-			if (e.type == EventType.BoardClicked) {
-				BoardClickedEvent f = (BoardClickedEvent) e;
-				if (card.getSelectionType() == SelectionType.HAND) {
-					HandClickedEvent g = (HandClickedEvent) e;
-					candidate = new HandSelection(g.player, g.cardClicked);
-				} else {
-					if (current == null)
-						candidate = selectionMap.get(card.getSelectionType())
-								.add(f.cardClicked);
-					else
-						candidate = current.add(f.cardClicked);
-				}
-				candidateRate = card.rateSelection(gameState, candidate);
-				if (candidateRate > 0) {
-
-					switch (card.getSelectionType()) {
-					case CELL:
-						if (current != null) {
-							Pair<Integer, Integer> pos = ((CellSelection) current).cell;
-							board.getCell(pos.first, pos.second).setHighlight(
-									false);
-						}
-						{
-							Pair<Integer, Integer> pos = ((CellSelection) candidate).cell;
-							board.getCell(pos.first, pos.second).setHighlight(
-									true);
-						}
-						break;
-					case COLUMN:
-						if (current != null) {
-							board.setColumnHighlight(
-									((ColumnSelection) current).column, false);
-						}
-						board.setColumnHighlight(
-								((ColumnSelection) candidate).column, true);
-						break;
-					case GROUP:
-						board.getCell(f.cardClicked.first, f.cardClicked.second)
-								.toggleHighlight();
-						break;
-					case HAND:
-						if (current != null) {
-							HandSelection pos = ((HandSelection) current);
-							gameState.gui.getHand(pos.player).getCell(pos.card)
-									.setHighlight(false);
-						}
-						HandSelection pos = ((HandSelection) candidate);
-						gameState.gui.getHand(pos.player).getCell(pos.card)
-								.setHighlight(true);
-						break;
+			candidateRate = card.rateSelection(gameState, candidate);
+			if (candidateRate > 0) {
+				switch (card.getSelectionType()) {
+				case CELL:
+					if (current != null) {
+						Pair<Integer, Integer> pos = ((CellSelection) current).cell;
+						board.getCell(pos.first, pos.second)
+								.setHighlight(false);
 					}
-					current = candidate;
-					currentRate = candidateRate;
+					{
+						Pair<Integer, Integer> pos = ((CellSelection) candidate).cell;
+						board.getCell(pos.first, pos.second).setHighlight(true);
+					}
+					break;
+				case COLUMN:
+					if (current != null) {
+						board.setColumnHighlight(
+								((ColumnSelection) current).column, false);
+					}
+					board.setColumnHighlight(
+							((ColumnSelection) candidate).column, true);
+					break;
+				case GROUP:
+					board.getCell(f.cardClicked.first, f.cardClicked.second)
+							.toggleHighlight();
+					break;
+				case HAND:
+					if (current != null) {
+						HandSelection pos = ((HandSelection) current);
+						gameState.gui.getHand(pos.player).getCell(pos.card)
+								.setHighlight(false);
+					}
+					HandSelection pos = ((HandSelection) candidate);
+					gameState.gui.getHand(pos.player).getCell(pos.card)
+							.setHighlight(true);
+					break;
 				}
+				current = candidate;
+				currentRate = candidateRate;
 			}
 		}
 	}
