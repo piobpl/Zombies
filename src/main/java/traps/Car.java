c835717b576bf4edc6ed12e063144b97bacfd3f8
jerzozwierz <jerzozwierz@jerzozwierz>
2013-05-15 21:06:09 +0200

    Car

---
+++ src/main/java/traps/Car.java
@@ -0,0 +1,80 @@
+package traps;
+
+import cards.helpers.DamageDealer;
+import cards.helpers.SolidityTester;
+import model.Card;
+import model.GameState;
+import model.Trap;
+import utility.Pair;
+/**
+ * Trap card. 
+ * @author jerzozwierz
+ *
+ */
+public class Car extends Trap {
+
+	public Car(GameState gameState, Pair<Integer, Integer> coordinates) {
+		coX = coordinates.first;
+		coY = coordinates.second;
+		this.gameState = gameState;
+		this.coordinates = coordinates;
+	}
+	
+	public final Integer coX;
+	public final Integer coY;
+	private final Pair<Integer, Integer> coordinates;
+	private GameState gameState;
+	
+	@Override
+	public String getName() {
+		return "Car";
+	}
+
+	@Override
+	public int getTime() {
+		return 10;
+	}
+	
+	@Override
+	public void decreaseTime() {}
+	//best solution, I guess
+
+	@Override
+	public boolean isMovePossible(Card card, Pair<Integer, Integer> from) {
+		return !card.getName().equals("Barrel");
+	}
+
+	@Override
+	public void movedOn(Card card) {}
+
+	@Override
+	public void shot() {
+		explode();
+		//TODO delete this shit
+	}
+
+	@Override
+	public void fire() {
+		explode();
+		//TODO delete this shit
+	}
+
+	@Override
+	public void blowUp() {
+		explode();
+		//TODO delete this shit
+	}
+	
+	private void explode() {
+		for (int i=0; i<5; i++) {
+			for (int j=0; j<3; j++) {
+				Pair<Integer, Integer> temp = new Pair<Integer, Integer>(i,j);
+				if (SolidityTester.areVertexAdjacent(temp, coordinates) ||
+						temp.equals(coordinates)) {
+					DamageDealer.dealExplosionDamage(gameState, i, j, 1);
+				}
+			}
+		}
+	}
+
+}
