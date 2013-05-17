15f30b4627f578b026eaa06a05eaef6ecb9a9711
Karol Rozycki <rozycki.karol@gmail.com>
2013-05-17 16:52:11 +0200

    + Mine

--- src/test/java/model/cards/MineTest.java
+++ src/test/java/model/cards/MineTest.java
@@ -19,7 +19,10 @@ public class MineTest {
 	public void test() {
 		Controller controller = new Controller();
 		Mine mine = new Mine();
-		assertEquals(2,mine.rateSelection(controller.gameState,ForTestsOnly.getNewCellSelection(3, 2)));
+		assertEquals(
+				2,
+				mine.rateSelection(controller.gameState,
+						ForTestsOnly.getNewCellSelection(3, 2)));
 		mine.makeEffect(ForTestsOnly.getNewCellSelection(3, 2),
 				controller.gameState);
 		int a = 0;
@@ -31,12 +34,24 @@ public class MineTest {
 			}
 		}
 		assertEquals(1, a);
-		assertEquals(0,mine.rateSelection(controller.gameState,ForTestsOnly.getNewCellSelection(3, 2)));
+		assertEquals(
+				0,
+				mine.rateSelection(controller.gameState,
+						ForTestsOnly.getNewCellSelection(3, 2)));
 		new Zombie(1).makeEffect(ForTestsOnly.getNewCellSelection(0, 0),
 				controller.gameState);
 		Mine secondmine = new Mine();
-		assertEquals(0,secondmine.rateSelection(controller.gameState,ForTestsOnly.getNewCellSelection(0, 0)));
-		assertEquals(0,secondmine.rateSelection(controller.gameState,ForTestsOnly.getNewCellSelection(1, 0)));
-		assertEquals(2,secondmine.rateSelection(controller.gameState,ForTestsOnly.getNewCellSelection(2, 0)));
+		assertEquals(
+				0,
+				secondmine.rateSelection(controller.gameState,
+						ForTestsOnly.getNewCellSelection(0, 0)));
+		assertEquals(
+				0,
+				secondmine.rateSelection(controller.gameState,
+						ForTestsOnly.getNewCellSelection(1, 0)));
+		assertEquals(
+				2,
+				secondmine.rateSelection(controller.gameState,
+						ForTestsOnly.getNewCellSelection(2, 0)));
 	}
 }
