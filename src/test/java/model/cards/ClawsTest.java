4c840f17c48c0de32b7a6529999b1a096a5ba044
Karol Rozycki <rozycki.karol@gmail.com>
2013-05-17 15:41:47 +0200

    Claws are ready.

---
+++ src/test/java/model/cards/ClawsTest.java
@@ -0,0 +1,40 @@
+package model.cards;
+
+import static org.junit.Assert.assertEquals;
+import model.cards.zombies.Claws;
+import model.cards.zombies.Zombie;
+
+import org.junit.Test;
+
+import controller.Controller;
+import controller.ForTestsOnly;
+import controller.Selection.CellSelection;
+
+public class ClawsTest {
+
+	@Test
+	public void test() {
+		CellSelection selection1 = ForTestsOnly.getNewCellSelection(1, 2);
+		CellSelection selection2 = ForTestsOnly.getNewCellSelection(2, 2);
+		CellSelection selection3 = ForTestsOnly.getNewCellSelection(4, 1);
+		CellSelection selection4 = ForTestsOnly.getNewCellSelection(3, 2);
+		Controller controller = new Controller();
+		Claws claws = new Claws();
+		new Zombie(1).makeEffect(ForTestsOnly.getNewCellSelection(4, 1),
+				controller.gameState);
+		new Zombie(2).makeEffect(ForTestsOnly.getNewCellSelection(3, 2),
+				controller.gameState);
+		assertEquals(0, claws.rateSelection(controller.gameState, selection1));
+		assertEquals(0, claws.rateSelection(controller.gameState, selection2));
+		assertEquals(2, claws.rateSelection(controller.gameState, selection3));
+		assertEquals(2, claws.rateSelection(controller.gameState, selection4));
+		int st1 = controller.gameState.getBoard().get(4, 1).getStrength();
+		int st2 = controller.gameState.getBoard().get(3, 2).getStrength();
+		claws.makeEffect(selection3, controller.gameState);
+		claws.makeEffect(selection4, controller.gameState);
+		assertEquals(st1 + 1, (int) controller.gameState.getBoard().get(4, 1)
+				.getStrength());
+		assertEquals(st2 + 1, (int) controller.gameState.getBoard().get(3, 2)
+				.getStrength());
+	}
+}
