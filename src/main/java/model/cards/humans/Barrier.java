e271067ada755ea21cbbbd3bfd909c29892595db
Edoipi <Edoipi@10.0.0.4>
2013-05-18 19:12:42 +0200

    Barrier

---
+++ src/main/java/model/cards/humans/Barrier.java
@@ -0,0 +1,48 @@
+package model.cards.humans;
+
+import model.GameState;
+import model.cards.helpers.Card;
+import model.traps.BarrierTrap;
+import controller.Selection;
+import controller.Selection.ColumnSelection;
+import controller.Selection.SelectionType;
+
+public class Barrier extends Card {
+
+	@Override
+	public int rateSelection(GameState gameState, Selection selection) {
+		Integer c=((ColumnSelection) selection).column;
+		if(!gameState.getBoard().isEmpty(4, c))
+			return 0;
+		return 2;
+	}
+
+	@Override
+	public void makeEffect(Selection selection, GameState gameState) {
+		Integer c=((ColumnSelection) selection).column;
+		for(int i=0; i<5; i++){
+			gameState.getBoard().getTraps(i, c).add(new BarrierTrap(4));
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "Barrier";
+	}
+
+	@Override
+	public SelectionType getSelectionType() {
+		return SelectionType.COLUMN;
+	}
+
+	@Override
+	public Integer getStrength() {
+		return null;
+	}
+
+	@Override
+	public void setStrength(Integer strength) {
+		throw new java.lang.UnsupportedOperationException();
+	}
+
+}
