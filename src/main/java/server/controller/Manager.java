f6e984685d0e91c3deb84876b027b0a08fba081e
michal <michal@zajac-komp>
2013-05-27 21:51:26 +0200

    Manager update

--- src/main/java/server/controller/Manager.java
+++ src/main/java/server/controller/Manager.java
@@ -1,6 +1,7 @@
 package server.controller;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.LinkedList;
@@ -12,39 +13,66 @@ import server.messages.SomeoneHasLoggedIn;
  * Klasa pomocnicza do wysyłania komunikatów graczom.
  * 
  * @author michal
- *
+ * 
  */
 public class Manager implements Runnable {
-	
+
 	private List<Socket> clients = new LinkedList<Socket>();
-	private List<ObjectOutputStream> streams;
+	private List<ObjectOutputStream> outputStreams;
 
 	public Manager() {
-		
+
 	}
 
-	// ta funkcja powinna byc "szybka" - wywoluje ja serwer, ktory nie powinien
-	// czekac
-	public void addClient(Socket client, SomeoneHasLoggedIn msg) {
-		for (ObjectOutputStream stream : streams)
+	public void sendAll(Message msg) {
+		for (ObjectOutputStream stream : outputStreams)
 			try {
 				stream.writeObject(msg);
 			} catch (IOException e) {
 				System.err.println("Nie moge pisac do strumienia " + stream);
 				e.printStackTrace();
 			}
+	}
+
+	// ta funkcja powinna byc "szybka" - wywoluje ja serwer, ktory nie powinien
+	// czekac
+	public void addClient(final Socket client, SomeoneHasLoggedIn msg) {
+		sendAll(msg);
 		clients.add(client);
 		try {
-			streams.add(new ObjectOutputStream(client.getOutputStream()));
+			outputStreams.add(new ObjectOutputStream(client.getOutputStream()));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Thread listener = new Thread(new Runnable() {
+				private ObjectInputStream inputStream = new ObjectInputStream(client.getInputStream());
+				@Override
+				public void run() {
+					Message msg = null;
+					while(true) {
+						try {
+							msg = (Message) (inputStream.readObject());
+						} catch (ClassNotFoundException | IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						sendAll(msg);
+					}
+				}
+			});
+			listener.setDaemon(true);
+			listener.start();
+		} catch (IOException e) {
+			System.err.println("Nie moge utworzyc watku do nasluchiwania!");
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public void run() {
-
+		
 	}
 
 }
