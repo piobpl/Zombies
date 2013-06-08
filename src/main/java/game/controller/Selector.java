3a24fc84731cc9d6197a822e3c527e2ffdedd72a
piobpl <piotrb10@gmail.com>
2013-06-08 20:46:26 +0200

    Akceptacja zagrania kart z cell selekcja za pomoca doubleclicka

--- src/main/java/game/controller/Selector.java
+++ src/main/java/game/controller/Selector.java
@@ -26,9 +26,9 @@ import java.util.EnumMap;
 import utility.Pair;
 
 /**
- * 
+ *
  * @author Edoipi, piob
- * 
+ *
  */
 public class Selector {
 	public final EventReceiver eventReceiver;
@@ -199,6 +199,9 @@ public class Selector {
 					}
 					current = candidate;
 					currentRate = candidateRate;
+					if ((card.getSelectionType() == SelectionType.CELL)
+							&& e.info.getClickCount() > 1 && currentRate == 2)
+						return current;
 				}
 			}
 		} finally {
