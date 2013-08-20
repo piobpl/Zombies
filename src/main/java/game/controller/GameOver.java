e03d6e9a1cb956c3c3f803f5454164da8da68196
krozycki <rozycki.karol@gmail.com>
2013-08-20 11:23:27 +0200

    Added one comment. Just for testing.

--- src/main/java/game/controller/GameOver.java
+++ src/main/java/game/controller/GameOver.java
@@ -2,6 +2,10 @@ package game.controller;
 
 import game.model.Player;
 
+/**
+ * A class that handles the end of the game.
+ */
+
 public class GameOver extends RuntimeException {
 
 	private static final long serialVersionUID = 5944458670405620920L;
